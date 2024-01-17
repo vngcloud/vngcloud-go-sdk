22a65d08ace944c86503d6bd0daa0f13b8f2fa42
andrew <an.ngdinh@gmail.com>
2024-01-17 14:34:27 +0700

    ⚡ update test cluster

--- vngcloud/services/coe/v2/cluster/coe_test.go
+++ vngcloud/services/coe/v2/cluster/coe_test.go
@@ -2,32 +2,60 @@ package cluster
 
 import (
 	"fmt"
+	"testing"
+
+	"github.com/vngcloud/vngcloud-go-sdk/client"
 	"github.com/vngcloud/vngcloud-go-sdk/vngcloud"
 	"github.com/vngcloud/vngcloud-go-sdk/vngcloud/services/identity/v2/extensions/oauth2"
 	"github.com/vngcloud/vngcloud-go-sdk/vngcloud/services/identity/v2/tokens"
-	"testing"
 )
 
-func TestCreate(t *testing.T) {
-	clientID := ""
-	clientSecret := ""
-	projectID := ""
-	clusterID := ""
+var (
+	projectID = ""
+	clusterID = ""
+)
 
-	identityURL := "https://iamapis.vngcloud.vn/accounts-api/v2"
+func NewSC() *client.ServiceClient {
+	var (
+		identityURL  = ""
+		vLbURL       = ""
+		clientID     = ""
+		clientSecret = ""
+	)
+
 	provider, _ := vngcloud.NewClient(identityURL)
 	vngcloud.Authenticate(provider, &oauth2.AuthOptions{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
 		AuthOptionsBuilder: &tokens.AuthOptions{
-			IdentityEndpoint: "https://iamapis.vngcloud.vn/accounts-api/v2",
+			IdentityEndpoint: identityURL,
 		},
 	})
 
-	vserver, _ := vngcloud.NewServiceClient(
-		"https://hcm-3.api.vngcloud.vn/vserver/vserver-gateway/v2",
+	vlb, _ := vngcloud.NewServiceClient(
+		vLbURL,
 		provider,
 		"vserver")
+	return vlb
+}
+
+func TestGet(t *testing.T) {
+	vserver := NewSC()
+
+	opts := new(GetOpts)
+	opts.ProjectID = projectID
+	opts.ClusterID = clusterID
+
+	resp, err := Get(vserver, opts)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
+
+	fmt.Printf("%v\n", resp)
+}
+
+func TestCreate(t *testing.T) {
+	vserver := NewSC()
 
 	opts := new(UpdateSecgroupOpts)
 	opts.ClusterID = clusterID
