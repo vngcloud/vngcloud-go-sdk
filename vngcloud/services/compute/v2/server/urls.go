9e1f713795bc89cf5ac7d26c2bdd0ea29f50a48b
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-01-31 10:26:48 +0700

    [chore] add newrequets and refactor url

--- vngcloud/services/compute/v2/server/urls.go
+++ vngcloud/services/compute/v2/server/urls.go
@@ -2,9 +2,9 @@ package server
 
 import "github.com/vngcloud/vngcloud-go-sdk/client"
 
-func getServerURL(sc *client.ServiceClient, opts GetOptsBuilder) string {
-	return sc.ServiceURL(
-		opts.ExtractProjectID(),
+func getServerURL(pSc *client.ServiceClient, pOpts IGetOptsBuilder) string {
+	return pSc.ServiceURL(
+		pOpts.GetProjectID(),
 		"servers",
-		opts.ExtractInstanceID())
+		pOpts.GetServerID())
 }
