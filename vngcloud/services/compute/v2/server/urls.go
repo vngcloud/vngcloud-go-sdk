c9d8d264162eab2dc34d08f59215e0bd8c16ca7c
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-02-01 10:39:48 +0700

    This is Manh Cuong's commit 🌻.

--- vngcloud/services/compute/v2/server/urls.go
+++ vngcloud/services/compute/v2/server/urls.go
@@ -1,8 +1,17 @@
 package server
 
-import "github.com/vngcloud/vngcloud-go-sdk/client"
+import (
+	"github.com/vngcloud/vngcloud-go-sdk/client"
+)
 
 func getServerURL(pSc *client.ServiceClient, pOpts IGetOptsBuilder) string {
+	return pSc.ServiceURL(
+		pOpts.GetProjectID(),
+		"servers",
+		pOpts.GetServerID())
+}
+
+func deleteServerURL(pSc *client.ServiceClient, pOpts IDeleteOptsBuilder) string {
 	return pSc.ServiceURL(
 		pOpts.GetProjectID(),
 		"servers",
