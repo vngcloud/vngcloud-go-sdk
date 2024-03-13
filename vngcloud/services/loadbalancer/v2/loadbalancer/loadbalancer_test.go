e27030fb0e8c6f9a5df046d3820b497fd937fe01
andrew <an.ngdinh@gmail.com>
2024-03-13 10:53:42 +0700

    🔨 fix test update lb

--- vngcloud/services/loadbalancer/v2/loadbalancer/loadbalancer_test.go
+++ vngcloud/services/loadbalancer/v2/loadbalancer/loadbalancer_test.go
@@ -93,17 +93,17 @@ func TestGet(t *testing.T) {
 	fmt.Printf("%+v\n", resp)
 }
 
-func TestResizeListener(t *testing.T) {
+func TestUpdate(t *testing.T) {
 	vlb := NewSC()
 
-	opt := &ResizeOpts{
-		PackageId: "lbp-f562b658-0fd4-4fa6-9c57-c1a803ccbf86",
-		// PackageId: "lbp-c531bc55-27d7-4a3e-be0b-eac265658a50",
+	opt := &UpdateOpts{
+		// PackageID: "lbp-f562b658-0fd4-4fa6-9c57-c1a803ccbf86",
+		PackageID: "lbp-c531bc55-27d7-4a3e-be0b-eac265658a50",
 	}
 	opt.ProjectID = projectID
 	opt.LoadBalancerID = loadbalancerID
 
-	err := Resize(vlb, opt)
+	_, err := Update(vlb, opt)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 	}
