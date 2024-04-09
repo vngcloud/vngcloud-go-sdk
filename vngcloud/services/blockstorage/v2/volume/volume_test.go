46d6bc0ee2c64fd596c96e46f6b54d2d45f40f05
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-04-09 15:18:05 +0700

    [chore] enhance api get volume

---
+++ vngcloud/services/blockstorage/v2/volume/volume_test.go
@@ -0,0 +1,57 @@
+package volume
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/vngcloud/vngcloud-go-sdk/client"
+	"github.com/vngcloud/vngcloud-go-sdk/vngcloud"
+	"github.com/vngcloud/vngcloud-go-sdk/vngcloud/services/identity/v2/extensions/oauth2"
+	"github.com/vngcloud/vngcloud-go-sdk/vngcloud/services/identity/v2/tokens"
+)
+
+func PointerOf[T any](t T) *T {
+	return &t
+}
+
+var (
+	projectID = "pro-462803f3-6858-466f-bf05-df2b33faa360"
+	volID     = "vol-a811dd64-5347-423b-ba8b-12570dcf5bc9"
+)
+
+func NewSC() *client.ServiceClient {
+	var (
+		identityURL  = "https://iamapis.vngcloud.vn/accounts-api/v2"
+		vServerURL   = "https://hcm-3.api.vngcloud.vn/vserver/vserver-gateway/v2"
+		clientID     = "cc136360-709c-4248-9358-e8e96c74480a"
+		clientSecret = "f5956359-26d5-4e9a-83f2-7f0bd6fabe28"
+	)
+
+	provider, _ := vngcloud.NewClient(identityURL)
+	vngcloud.Authenticate(provider, &oauth2.AuthOptions{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		AuthOptionsBuilder: &tokens.AuthOptions{
+			IdentityEndpoint: identityURL,
+		},
+	})
+
+	vlb, _ := vngcloud.NewServiceClient(
+		vServerURL,
+		provider,
+		"vlb")
+	return vlb
+}
+
+func TestGetVolume(t *testing.T) {
+	vserverClient := NewSC()
+
+	opt := NewGetOpts(projectID, volID)
+
+	resp, err := Get(vserverClient, opt)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	fmt.Printf("resp: %+v\n", resp)
+}
