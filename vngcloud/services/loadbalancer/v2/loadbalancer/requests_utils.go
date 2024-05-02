25f584c194f1193d28ef25ede4e1bd133a08f55c
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-05-02 17:05:15 +0700

    [chore] api create lb tag

--- vngcloud/services/loadbalancer/v2/loadbalancer/requests_utils.go
+++ vngcloud/services/loadbalancer/v2/loadbalancer/requests_utils.go
@@ -37,3 +37,23 @@ func NewUpdateOpts(pProjectID, pLoadBalancerID string, pOpts *UpdateOpts) IUpdat
 	pOpts.LoadBalancerID = pLoadBalancerID
 	return pOpts
 }
+
+func NewCreateTagOpts(pprojectID, plbId string, ptags map[string]string) ICreateTagOptsBuilder {
+	opts := new(CreateTagOpts)
+	opts.ProjectID = pprojectID
+	opts.LoadBalancerID = plbId
+	opts.ResourceType = "LOAD-BALANCER"
+	opts.ResourceID = plbId
+
+	if ptags != nil {
+		opts.TagRequestList = make([]CreateTagOptsTag, 0)
+		for key, value := range ptags {
+			opts.TagRequestList = append(opts.TagRequestList, CreateTagOptsTag{
+				Key:   key,
+				Value: value,
+			})
+		}
+	}
+
+	return opts
+}
