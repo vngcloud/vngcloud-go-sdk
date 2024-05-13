1ee81d02137c0c0d9908a6b96614117e0a29424e
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-05-13 12:27:39 +0700

    [build][chore] update error handler for api attach volume to server and update github action

--- vngcloud/errors/server.go
+++ vngcloud/errors/server.go
@@ -6,11 +6,13 @@ import (
 )
 
 const (
-	patternOutOfPoc = "you do not have sufficient credits to complete the purchase"
+	patternOutOfPoc                        = "you do not have sufficient credits to complete the purchase"
+	patternServerAttachVolumeQuotaExceeded = "exceeded volume_per_server quota" // "Exceeded VOLUME_PER_SERVER quota. Current used: 10, max: 10."
 )
 
 var (
-	ErrCodeOutOfPoc lssdkErr.ErrorCode = "ErrorOutOfPoc"
+	ErrCodeOutOfPoc                        lssdkErr.ErrorCode = "ErrorOutOfPoc"
+	ErrCodeServerAttachVolumeQuotaExceeded lssdkErr.ErrorCode = "ErrorServerAttachVolumeQuotaExceeded"
 )
 
 func WithErrorOutOfPoc(perrResp *lssdkErr.ErrorResponse, perr error) func(*lssdkErr.SdkError) {
@@ -22,6 +24,21 @@ func WithErrorOutOfPoc(perrResp *lssdkErr.ErrorResponse, perr error) func(*lssdk
 		errMsg := perrResp.Message
 		if lstr.Contains(lstr.ToLower(lstr.TrimSpace(errMsg)), patternOutOfPoc) {
 			sdkError.Code = ErrCodeOutOfPoc
+			sdkError.Message = errMsg
+			sdkError.Error = perr
+		}
+	}
+}
+
+func WithErrorServerAttachVolumeQuotaExceeded(perrResp *lssdkErr.ErrorResponse, perr error) func(*lssdkErr.SdkError) {
+	return func(sdkError *lssdkErr.SdkError) {
+		if perrResp == nil {
+			return
+		}
+
+		errMsg := perrResp.Message
+		if lstr.Contains(lstr.ToLower(lstr.TrimSpace(errMsg)), patternServerAttachVolumeQuotaExceeded) {
+			sdkError.Code = ErrCodeServerAttachVolumeQuotaExceeded
 			sdkError.Message = errMsg
 			sdkError.Error = perr
 		}
