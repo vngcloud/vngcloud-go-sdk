cbf52fa1573937abd5bba686ca67f28adc7fe5cb
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-08-30 16:23:11 +0700

    [chore] update list tag by load balancer ID api

--- vngcloud/services/loadbalancer/v2/tag_response.go
+++ vngcloud/services/loadbalancer/v2/tag_response.go
@@ -6,14 +6,16 @@ type ListTagResponse struct {
 	Key       string `json:"key"`
 	Value     string `json:"value"`
 	CreatedAt string `json:"createdAt"`
+	SystemTag bool   `json:"systemTag,omitempty"`
 }
 
 type ListTagsResponse []ListTagResponse
 
 func (s ListTagResponse) ToEntityTag() *lsentity.Tag {
 	return &lsentity.Tag{
-		Key:   s.Key,
-		Value: s.Value,
+		Key:       s.Key,
+		Value:     s.Value,
+		SystemTag: s.SystemTag,
 	}
 }
 
