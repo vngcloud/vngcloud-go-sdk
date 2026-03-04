e3ff07288b853f811e183457de9fc8fb8b1839b0
tytv2 <tytv2@vng.com.vn>
2026-03-04 14:44:19 +0700

    fix: reduce timeout duration in HTTP client configuration

--- vngcloud/client/http.go
+++ vngcloud/client/http.go
@@ -53,7 +53,7 @@ func NewHttpClient(pctx lctx.Context) IHttpClient {
 		client: lreq.NewClient().
 			SetCommonRetryCount(3).
 			SetCommonRetryFixedInterval(10).
-			SetTimeout(ljtime.Second(120)),
+			SetTimeout(ljtime.Second(1)),
 		mut:       new(lsync.RWMutex),
 		reauthmut: new(reauthlock),
 	}
